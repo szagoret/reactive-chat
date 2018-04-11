7a5923b904faa6eea87cdedc4e0cb6defab0cadb
Sergiu Zagoret <zagoret@outlook.com>
2018-04-11 14:34:46 +0200

    Setup queue properties, reformat and optimize code

--- src/main/java/com/szagoret/springboot2/chat/ChatApplication.java
+++ src/main/java/com/szagoret/springboot2/chat/ChatApplication.java
@@ -1,14 +1,14 @@
 package com.szagoret.springboot2.chat;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableEurekaClient
 public class ChatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
 }
