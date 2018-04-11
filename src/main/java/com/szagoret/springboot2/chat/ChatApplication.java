62f89f5bebbb38388e5962acbbc86aaaceeedc6f
Sergiu Zagoret <sergiuzagoret@MB-Zagoret01.qintern.de>
2018-04-11 14:28:55 +0200

    Add initial files

---
+++ src/main/java/com/szagoret/springboot2/chat/ChatApplication.java
@@ -0,0 +1,14 @@
+package com.szagoret.springboot2.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+}
